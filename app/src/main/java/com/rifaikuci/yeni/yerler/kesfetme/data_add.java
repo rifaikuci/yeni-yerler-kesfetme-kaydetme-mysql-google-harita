02aaf0b26dfc3fd36fb4924f155a15704b4b4030
Rifai Kuçi <rfai4747@hotmail.com>
2020-01-08 13:37:02 +0300

    data ekleme kısmı design  scroolview ve  fotoğraf seçme

---
+++ app/src/main/java/com/rifaikuci/yeni/yerler/kesfetme/data_add.java
@@ -0,0 +1,14 @@
+package com.rifaikuci.yeni.yerler.kesfetme;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class data_add extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_data_add);
+    }
+}
